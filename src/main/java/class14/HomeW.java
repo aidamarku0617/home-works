f016b4c6a74e80964d56704be7e2f4c3a9639d42
aidamarku0617 <aidamarku0617@gmail.com>
2023-12-09 20:01:03 -0500

    class14

---
+++ src/main/java/class14/HomeW.java
@@ -0,0 +1,11 @@
+package class14;
+
+public class HomeW {
+    //Create a method that will accept an array as parameters and will return
+    // a sum of all elements from that array.
+    // Method should be visibly only within same package and accessible
+    // by the creating an instance/object of the class.
+
+
+
+}
